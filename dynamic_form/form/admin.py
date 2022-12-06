1ae615e218a92333aafb70347a63d467ce36de91
Somesh Singh <someshs@adobe.com>
2022-12-07 02:34:28 +0530

    Updated Multiple Questions

--- dynamic_form/form/admin.py
+++ dynamic_form/form/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Brand
+from .models import Brand, AppriseMethod
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
@@ -10,8 +10,18 @@ class BrandResource(resources.ModelResource):
         model = Brand
 
 
+class AppriseMethodResource(resources.ModelResource):
+    class Meta:
+        model = AppriseMethod
+
+
 class BrandAdmin(ImportExportModelAdmin):
     resource_class = BrandResource
 
 
+class AppriseMethodAdmin(ImportExportModelAdmin):
+    resource_class = AppriseMethodResource
+
+
 admin.site.register(Brand, BrandAdmin)
+admin.site.register(AppriseMethod, AppriseMethodAdmin)
--- dynamic_form/form/models.py
+++ dynamic_form/form/models.py
@@ -12,16 +12,36 @@ class Brand(models.Model):
         return self.name
 
 
+class AppriseMethod(models.Model):
+    method = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.method
+
+
 class Survey(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    seen_brands = models.ManyToManyField(Brand, related_name='seen')
-    produse_brands = models.ManyToManyField(Brand, related_name='produse')
-    pastuse_brands = models.ManyToManyField(Brand, related_name='pastuse')
+    seen_brands = models.ManyToManyField(Brand, related_name="seen")
+    produse_brands = models.ManyToManyField(Brand, related_name="produse")
+    pastuse_brands = models.ManyToManyField(Brand, related_name="pastuse")
     ad_blocking = models.BooleanField()
     yt_sub = models.BooleanField()
+    youtube_percentage = models.IntegerField()
+    apprise_methods = models.ManyToManyField(AppriseMethod)
 
 
 class SurveyForm(forms.ModelForm):
+    YOUTUBE_PERCENTAGE_CHOICES = [
+        (1, "<10% on mobile"),
+        (2, ">10% but <30% on mobile"),
+        (3, ">30% but <70% on mobile"),
+        (4, ">70% on mobile"),
+    ]
+    youtube_percentage = forms.ChoiceField(
+        choices=YOUTUBE_PERCENTAGE_CHOICES,
+        widget=forms.RadioSelect,
+    )
+
     class Meta:
         model = Survey
         fields = [
@@ -30,13 +50,16 @@ class SurveyForm(forms.ModelForm):
             "pastuse_brands",
             "ad_blocking",
             "yt_sub",
-            ]
+            "youtube_percentage",
+            "apprise_methods",
+        ]
         widgets = {
             "seen_brands": forms.CheckboxSelectMultiple,
             "produse_brands": forms.CheckboxSelectMultiple,
             "pastuse_brands": forms.CheckboxSelectMultiple,
             "ad_blocking": forms.CheckboxInput,
             "yt_sub": forms.CheckboxInput,
+            "apprise_methods": forms.CheckboxSelectMultiple,
         }
         labels = {
             "seen_brands": "I remember seeing ads for the following brands this year",
@@ -45,4 +68,6 @@ class SurveyForm(forms.ModelForm):
             "ad_blocking": "I use ad blocking software",
             "yt_sub": "Do you use a Youtube subscription?",
             "nouse_brands": "I have never used these brands",
+            "youtube_percentage": "Approximately how much percentage of time do you spend on Youtube mobile vs Youtube web?",
+            "apprise_methods": "How do you apprise yourself of the latest products and brands?",
         }
